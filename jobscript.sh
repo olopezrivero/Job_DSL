4b9d98e4c8d1f2aca1214cd56c86a66d4434285b
olopezrivero <119618239+olopezrivero@users.noreply.github.com>
2024-07-22 08:56:50 -0300

    Create jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Bienvenido/a al planeta $planeta"
+if [ "$agente" = "true" ]
+then
+	echo "Preparese para la mision"
+else
+	echo "Disfruta el planeta humano/a $nombre, trata de no morir"
+fi
+echo "..."
+sleep 10
+echo "!suerte la necesitaras!"
